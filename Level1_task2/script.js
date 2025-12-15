6836c4200818bed37a795fa4e35603b18464cf49
Anele0663 <ngubaneanele22@gmail.com>
2025-12-15 05:58:37 +0200

    Add files via upload

---
+++ Level1_task2/script.js
@@ -0,0 +1,7 @@
+// Mobile Menu
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+});
